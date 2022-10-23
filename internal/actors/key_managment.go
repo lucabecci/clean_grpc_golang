ef0ea85d23d8748038c78f2f212dbf9e3a00938e
lucabecci <beccibrian@gmail.com>
2022-10-23 16:10:35 -0300

    feat(All): protos and storage

--- internal/actors/key_managment.go
+++ internal/actors/key_managment.go
@@ -1,4 +1,4 @@
-package core
+package actors
 
 type KeyManagment struct {
 	Managment string
--- internal/core/static_managment.go
+++ internal/core/static_managment.go
@@ -1,11 +1,27 @@
 package core
 
-var managment []int
+type StaticManagment struct {
+	Storage map[string]int
+}
 
-type StaticManagment struct{}
+type IStaticManagment interface {
+}
+
+var managment StaticManagment
 
 func InstanceStaticManagment() *StaticManagment {
-	if managment == nil {
-		management = {}
+	if managment.Storage == nil {
+		managment = StaticManagment{
+			Storage: map[string]int{},
+		}
 	}
+	return &managment
+}
+
+func (s *StaticManagment) Create(key string, value string) (int64, error) {
+	return 0, nil
+}
+
+func (s *StaticManagment) Get(key string) (interface{}, error) {
+	return nil, nil
 }
